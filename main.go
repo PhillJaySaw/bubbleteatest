6f7f9b72a918b18b78f958e03e71cc678a3334cc
philliplawniczak <philljaysaw@gmail.com>
2024-07-22 08:31:46 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type task struct {
+	id      int
+	content string
+}
+
+type model struct {
+	choices  []task
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initializeModel() model {
+	tasks := []task{
+		{
+			id:      1,
+			content: "Go running",
+		},
+		{
+			id:      2,
+			content: "Groceries",
+		},
+		{
+			id:      3,
+			content: "Do the dishes",
+		},
+	}
+
+	return model{
+		choices:  tasks,
+		selected: make(map[int]struct{}),
+		cursor:   tasks[0].id,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+
+		switch msg.String() {
+
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "down", "j":
+			m.Next()
+		case "up", "k":
+			m.Prev()
+
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		case "a":
+			id := rand.IntN(10000) + 1
+			m.choices = append(m.choices, task{id: id, content: "task"})
+
+			if m.cursor == 0 {
+				m.cursor = m.choices[0].id
+			}
+		case "d":
+			var indexToRemove int
+			for i, choice := range m.choices {
+				if choice.id == m.cursor {
+					indexToRemove = i
+					break
+				}
+			}
+			copy(m.choices[indexToRemove:], m.choices[indexToRemove+1:])
+			m.choices = m.choices[:len(m.choices)-1]
+
+			if len(m.choices) == 0 {
+				m.cursor = 0
+			} else if indexToRemove == len(m.choices) {
+				m.cursor = m.choices[len(m.choices)-1].id
+			} else if indexToRemove == 0 {
+				m.cursor = m.choices[0].id
+			} else {
+				m.cursor = m.choices[indexToRemove].id
+			}
+		}
+
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "What do you need to do?\n\n"
+
+	for _, choice := range m.choices {
+		cursor := " "
+		if m.cursor == choice.id {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[choice.id]; ok {
+			checked = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice.content)
+
+	}
+
+	if len(m.choices) == 0 {
+		s += fmt.Sprintf("No tasks to dispaly\n")
+	}
+
+	s += "\nj/k - up/down | a - add | space - check | d - delete\n"
+
+	return s
+}
+
+func (m *model) Next() {
+	for i, task := range m.choices {
+		if task.id == m.cursor {
+			if i+1 < len(m.choices) {
+				m.cursor = m.choices[i+1].id
+				break
+			}
+		}
+	}
+}
+
+func (m *model) Prev() {
+	for i, task := range m.choices {
+		if task.id == m.cursor {
+			if i-1 >= 0 {
+				m.cursor = m.choices[i-1].id
+				break
+			}
+			break
+		}
+	}
+}
+
+func main() {
+	p := tea.NewProgram(initializeModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error occured while trying to run the program: %v", err)
+		os.Exit(1)
+	}
+}
